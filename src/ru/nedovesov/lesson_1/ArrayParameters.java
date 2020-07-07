4efe898ca24cb7113014e68cb60e18fcb2a516a2
NedKonstantin <slavkosnet@mail.ru>
2020-07-07 23:03:56 +0300

    Проверена работа метода

--- src/ru/nedovesov/lesson_1/ArrayParameters.java
+++ src/ru/nedovesov/lesson_1/ArrayParameters.java
@@ -1,4 +1,27 @@
 package ru.nedovesov.lesson_1;
 
-public class ArrayParameters {
+public class ArrayParameters<T> {
+    private T[] arr;
+
+    public ArrayParameters(T... arr) {
+        this.arr = arr;
+    }
+
+    public T[] getArr() {
+        return arr;
+    }
+// Метод, меняющий две переменных местами
+    public void changePlaceElements(int i, int j) {
+        if (checkIndex(i, j)) {
+            T temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        } else {
+            System.out.println("Incorrect index!");
+        }
+    }
+
+    private boolean checkIndex(int i, int j) {
+        return i >= 0 && i < arr.length && j >= 0 && j < arr.length && i != j;
+    }
 }
--- src/ru/nedovesov/lesson_1/Main.java
+++ src/ru/nedovesov/lesson_1/Main.java
@@ -1,4 +1,12 @@
 package ru.nedovesov.lesson_1;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayParameters<Integer> firstObj = new ArrayParameters<Integer>(20, 30, 40, 54, 24, 12, 33);
+        System.out.println(Arrays.toString(firstObj.getArr()));
+        firstObj.changePlaceElements(2, 0);
+        System.out.println(Arrays.toString(firstObj.getArr()));
+    }
 }
